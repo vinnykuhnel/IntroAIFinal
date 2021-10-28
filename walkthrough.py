aa9f85c9f6840a4dfe6637fc66b833965b69c2d4
vinnykuhnel <vak58@msstate.edu>
2021-10-27 21:14:35 -0500

    A little progress

---
+++ walkthrough.py
@@ -0,0 +1,20 @@
+from numpy import ndarray
+from sklearn import datasets, neighbors
+
+def kNearest(irisData: ndarray):
+    kn = neighbors.KNeighborsClassifier(n_neighbors=1)
+    kn.fit(irisData.data, irisData.target)
+
+
+
+
+
+
+
+
+
+
+
+irisData = datasets.load_iris()
+kNearest(irisData)
+
